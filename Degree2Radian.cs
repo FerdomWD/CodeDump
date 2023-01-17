6b6c8cf8e29d230386f71ca11bc47b2abb796a07
Ef.P <116003346+FerdomWD@users.noreply.github.com>
2023-01-17 18:05:49 +0200

    Thingy in c# that turns angle degrees to radians
    also rounds up the (0,1,2,3) 4th digit

---
+++ Degree2Radian.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your angle: ");
+        int degrees = Convert.ToInt32(Console.ReadLine());
+
+        double radians = degrees * (Math.PI / 180);
+        double roundedNumber = Math.Round(radians, 3);
+        string output = roundedNumber.ToString();
+
+        Console.WriteLine("Radians: " + output);
+
+        Console.ReadLine();
+
+
+    }
+
+
+}
